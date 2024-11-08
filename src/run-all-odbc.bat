551a5623f374fc89034e9451d8b198a89a38a8a7
charlie buckheit <charlie.buckheit@mercurysolutions.com>
2024-11-08 19:49:12 -0300

    run-all-odbc.bat tweak

--- src/run-all-odbc.bat
+++ src/run-all-odbc.bat
@@ -15,13 +15,14 @@ for /R ..\test %%i in (*.test) do sqllogictest --odbc "DSN=SQLite3 Datasource;DA
     set /a errors+=!ERRORLEVEL!
     @echo total errors: !errors!
 )
-@echo COMPLETE total errors: !errors!
 
 REM Expecting 2 integer overflow errors in "G:\sqllogictest\test\evidence\slt_lang_aggfunc.test" 
 REM According to the test file, Total() never throws an integer overflow. However with ODBC it throws error
 REM and returns value.
 REM We want to ignore these two errors so workflow passes. 
 set /a errors-=2
+
+@echo COMPLETE total errors: !errors!
 
 exit /b !errors!
 
